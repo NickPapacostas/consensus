2552b140cd9772a3c8f0d2fc7b2ff39f1124837b
t3hronin <np1249a@gmail.com>
2012-12-07 03:31:10 -0500

    rails controllers setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,15 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+  def require_user
+    unless current_user
+      format.html  {
+        flash[:error] = "You must be logged in to access this page"
+        redirect_to root_url
+      }
+    end
+  end
+
   helper_method :current_user
 end
