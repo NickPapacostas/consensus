cce54dec375964f1b1108599b3bb73c9fdf2a212
t3hronin <np1249a@gmail.com>
2012-12-13 00:38:03 -0500

    user required for viewing users index show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,22 +2,41 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    if current_user
+      @users = User.all
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @users }
+      respond_to do |format|
+        format.html # index.html.erb
+        format.json { render json: @users }
+      end
+    else
+      respond_to do |format|
+        format.html  {
+          flash[:error] = "You must be logged in to access this page"
+          redirect_to root_url
+        }
+      end
     end
+
   end
 
   # GET /users/1
   # GET /users/1.json
   def show
-    @user = User.find(params[:id])
+    if current_user
+      @user = User.find(params[:id])
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @user }
+      respond_to do |format|
+        format.html # show.html.erb
+        format.json { render json: @user }
+      end
+    else
+      respond_to do |format|
+        format.html  {
+          flash[:error] = "You must be logged in to access this page"
+          redirect_to root_url
+        }
+      end
     end
   end
 
