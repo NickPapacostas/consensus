6d753c500a783ce5b2d720e8d4347bbbb34bdf31
t3hronin <np1249a@gmail.com>
2012-12-13 13:50:30 -0500

    changed message and channel controllers

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,29 +1,92 @@
 class ChannelsController < ApplicationController
-  before_filter :require_user
-  before_filter :find_channel, :only => [:update, :destroy]
-
+  # GET /Channels
+  # GET /Channels.json
   def index
     @channels = Channel.all
-    render :json => @channels
-  end
 
-  def create
-    Channel.create!(params)
-    head :ok
-  end
-
-  def update
-    @channel.update_attributes!(params)
-    head :ok
-  end
-
-  def destroy
-    @channel.destroy
-    head :ok
-  end
-
-  private
-    def find_channel
-      @channel = Channel.find(params[:id])
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @channels }
     end
+  end
+
+  # GET /Channels/1
+  # GET /Channels/1.json
+  def show
+    @channel = Channel.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @channel }
+    end
+  end
+
+  # GET /Channels/new
+  # GET /Channels/new.json
+  def new
+    if current_user
+      @channel = Channel.new
+
+      respond_to do |format|
+        format.html # new.html.erb
+        format.json { render json: @channel }
+      end
+    else
+      flash[:error] = "Sign in to create an Channel"
+      respond_to do |format|
+        format.html{redirect_to root_url}
+      end
+    end
+
+  end
+
+  # GET /Channels/1/edit
+  def edit
+    @channel = Channel.find(params[:id])
+  end
+
+  # POST /Channels
+  # POST /Channels.json
+  def create
+    params[:channel][:id] = nil
+    @channel = Channel.new(params[:channel])
+
+    respond_to do |format|
+      if @channel.save
+        format.html { redirect_to @channel, notice: 'Channel was successfully created.' }
+        format.json { render json: @channel, status: :created, location: @channel }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @channel.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /Channels/1
+  # PUT /Channels/1.json
+  def update
+    @channel = Channel.find(params[:id])
+
+    respond_to do |format|
+      if @channel.update_attributes(params[:channel])
+        format.html { redirect_to @channel, notice: 'Channel was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @channel.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /Channels/1
+  # DELETE /Channels/1.json
+  def destroy
+    @channel = Channel.find(params[:id])
+    @channel.destroy
+
+    respond_to do |format|
+      format.html { redirect_to channels_url }
+      format.json { head :no_content }
+    end
+  end
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,22 +1,92 @@
 class MessagesController < ApplicationController
-  before_filter :require_user
-
+  # GET /Messages
+  # GET /Messages.json
   def index
     @messages = Message.all
-    render :json => @messages
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @messages }
+    end
   end
 
+  # GET /Messages/1
+  # GET /Messages/1.json
+  def show
+    @message = Message.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @message }
+    end
+  end
+
+  # GET /Messages/new
+  # GET /Messages/new.json
+  def new
+    if current_user
+      @message = Message.new
+
+      respond_to do |format|
+        format.html # new.html.erb
+        format.json { render json: @message }
+      end
+    else
+      flash[:error] = "Sign in to create an Message"
+      respond_to do |format|
+        format.html{redirect_to root_url}
+      end
+    end
+
+  end
+
+  # GET /Messages/1/edit
+  def edit
+    @message = Message.find(params[:id])
+  end
+
+  # POST /Messages
+  # POST /Messages.json
   def create
-    message = Message.new(params)
-    message.name = current_user
-    message.save!
-    head :ok
+    params[:message][:id] = nil
+    @message = Message.new(params[:message])
+
+    respond_to do |format|
+      if @message.save
+        format.html { redirect_to @message, notice: 'Message was successfully created.' }
+        format.json { render json: @message, status: :created, location: @message }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @message.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
+  # PUT /Messages/1
+  # PUT /Messages/1.json
   def update
-    message = Message.find(params[:id])
-    message.name = current_user
-    message.update_attributes!(params)
-    head :ok
+    @message = Message.find(params[:id])
+
+    respond_to do |format|
+      if @message.update_attributes(params[:message])
+        format.html { redirect_to @message, notice: 'Message was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @message.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /Messages/1
+  # DELETE /Messages/1.json
+  def destroy
+    @message = Message.find(params[:id])
+    @message.destroy
+
+    respond_to do |format|
+      format.html { redirect_to messages_url }
+      format.json { head :no_content }
+    end
   end
 end
