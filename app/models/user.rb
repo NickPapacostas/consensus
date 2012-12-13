2c64f45046ce7f98931d4eabf9b1495785232847
t3hronin <np1249a@gmail.com>
2012-12-13 00:04:37 -0500

    unique emails required

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
   has_many :messages, :through => :meetings
 
   validates_presence_of :password, :on => :create
+  validates_uniqueness_of :email
 
 
   def self.authenticate(email, password)
